77873f8cb6a5f308354a30ae3e71f9fcc7db6ccc
Joshua Hodson <j.c.hodson@live.com>
2017-05-29 19:43:10 +1000

    t2r Initial

--- sub_one/transforms.py
+++ sub_one/transforms.py
@@ -70,3 +70,18 @@ def r2t( rmat ):
         return np.c_[ tmp, np.array([[0],[0],[0],[1]]) ]
     else:
         raise ValueError(' Value must be a rotation matrix ')
+#---------------------------------------------------------------------------------------#
+def t2r( tmat ):
+    """ t2r, convert a given homogeneous transform to a rotation matrix """
+    dim = np.ndarray.shape(tmat)
+    if dim(0) != dim(1):
+        raise ValueError(' Matrix Must be square ')
+    elif dim(0) == 3:
+        tmp = numpy.delete(tmat, (2), axis=0)
+        return numpy.delete(tmp, (2), axis=1)
+    elif dim(0) == 4:  
+        tmp = numpy.delete(tmat, (3), axis=0)
+        return numpy.delete(tmp, (3), axis=1)
+    else:
+        raise ValueError(' Value must be a rotation matrix ')
+    
