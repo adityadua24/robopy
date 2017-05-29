67a0cb59c39e75fc88ae7b3a4962f720ac7f6e67
Joshua Hodson <j.c.hodson@live.com>
2017-05-29 17:56:20 +1000

    task 1.1.3 adjustment
    updated type to matrix

--- sub_one/transforms.py
+++ sub_one/transforms.py
@@ -70,9 +70,9 @@ def r2t( rmat ):
         raise ValueError(' Matrix Must be square ')
     elif dim(0) == 2:
         tmp = np.r_[rmat, np.zeros((1,2))]
-        return np.c_[ tmp, np.array([[0],[0],[1]]) ]
+        return np.c_[ tmp, np.matrix([[0],[0],[1]]) ]
     elif dim(0) == 3:  
         tmp = np.r_[rmat, np.zeros((1,3))]
-        return np.c_[ tmp, np.array([[0],[0],[0],[1]]) ]
+        return np.c_[ tmp, np.matrix([[0],[0],[0],[1]]) ]
     else:
         raise ValueError(' Value must be a rotation matrix ')
