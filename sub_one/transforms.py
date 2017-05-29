49016afa1ebb2bda7b76ffb7fe139b51f476be49
Joshua Hodson <j.c.hodson@live.com>
2017-05-29 17:45:35 +1000

    task 1.1.3 r2t
    initial r2t work

--- sub_one/transforms.py
+++ sub_one/transforms.py
@@ -63,3 +63,16 @@ def trotz(t, units="rad"):
     tm = np.r_[tm, np.zeros((1,3))]
     return np.c_[ tm, np.array([[0],[0],[0],[1]]) ]
 #---------------------------------------------------------------------------------------#
+def r2t( rmat ):
+    """ r2t, convert rotation matrix to a homogeneous transform """
+    dim = np.ndarray.shape(rmat)
+    if dim(0) != dim(1):
+        raise ValueError(' Matrix Must be square ')
+    elif dim(0) == 2:
+        tmp = np.r_[rmat, np.zeros((1,2))]
+        return np.c_[ tmp, np.array([[0],[0],[1]]) ]
+    elif dim(0) == 3:  
+        tmp = np.r_[rmat, np.zeros((1,3))]
+        return np.c_[ tmp, np.array([[0],[0],[0],[1]]) ]
+    else:
+        raise ValueError(' Value must be a rotation matrix ')
