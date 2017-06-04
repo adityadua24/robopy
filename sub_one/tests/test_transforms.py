c56811ae108be59d6a19ce29f12b1dca90112260
Aditya Dua <adityadua24@gmail.com>
2017-06-05 02:24:53 +1000

    Added import for numpy.testing Partially empty test suite added for rotx

--- sub_one/tests/test_transforms.py
+++ sub_one/tests/test_transforms.py
@@ -2,6 +2,7 @@ import unittest
 from math import pi
 from .. import transforms
 import numpy as np
+import numpy.testing as npt
 
 
 class TestRotx(unittest.TestCase):
@@ -13,8 +14,47 @@ class TestRotx(unittest.TestCase):
     def setUp(self):
         self.angle_rad = pi
 
-    def test_returns_npmatrix(self):
-        self.assertTrue(isinstance(transforms.rotx(self.angle_rad), np.matrix))
+    def test_validData_returnData_datatype(self):
+        self.assertIsInstance(transforms.rotx(self.angle_rad), np.matrix)
+
+    def test_validData_returnData_dimension(self):
+        dimensions = transforms.rotx(self.angle_rad).shape
+        self.assertEqual(dimensions, (3, 3))
+
+    def test_validData_boundaryCondition_0_rad(self):
+        expected_mat = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        # self.assertAlmostEqual(expected_mat, transforms.rotx(0))
+        npt.assert_almost_equal(expected_mat, transforms.rotx(0))
+
+    def test_validData_boundaryCondition_2pi_rad(self):
+        self.assertTrue(True)
+
+    def test_validData_boundaryCondition_2pi_multiple_rad(self):
+        self.assertTrue(True)
+
+    def test_validData_boundaryCondition_0_deg(self):
+        self.assertTrue(True)
+
+    def test_validData_boundaryCondition_360_deg(self):
+        self.assertTrue(True)
+
+    def test_validData_boundaryCondition_360_multiple_deg(self):
+        self.assertTrue(True)
+
+    def test_invalidData_arg1_string(self):
+        self.assertTrue(True)
+
+    def test_invalidData_arg1_bool(self):
+        self.assertTrue(True)
+
+    def test_invalidData_arg2_string_mismatch(self):
+        self.assertTrue(True)
+
+    def test_invalidData_arg2_bool(self):
+        self.assertTrue(True)
+
+    def test_invalidData_arg2_int(self):
+        self.assertTrue(True)
 
 
 if __name__ == "__main__":
