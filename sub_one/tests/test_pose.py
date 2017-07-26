14134c3b453eba902213b47867cdd3bc8e99af4a
Aditya Dua <adityadua24@gmail.com>
2017-07-26 12:40:13 +1000

    SO3 Test suite conflicts resolved. New specs added

--- sub_one/tests/test_pose.py
+++ sub_one/tests/test_pose.py
@@ -8,9 +8,11 @@ import numpy as np
 from .. import pose
 from .common import matrix_mismatch_string_builder
 from .common import matrices_equal
+from .. import transforms as tr
 
 
 class TestSO3(unittest.TestCase):
+    # TODO Validate tests on matrices other than identity as well!!
 
     def test_pose_so3_constructor_no_args(self):
         obj = pose.SO3()
@@ -20,18 +22,45 @@ class TestSO3(unittest.TestCase):
             output_str = matrix_mismatch_string_builder(rec_mat, exp_mat)
             self.fail(output_str)
 
-    def test_pose_so3_constructor_so3_object(self):
-        obj1 = pose.SO3()
+    def test_pose_so3_constructor_so3_object_length(self):
+        obj1 = pose.SO3()  # TODO
         obj2 = pose.SO3(obj1)
+        self.assertEquals(obj1.length, obj2.length)
 
+    def test_pose_so3_constructor_so3_object_data(self):
+        obj1 = pose.SO3()  # TODO
+        obj2 = pose.SO3(obj1)
         for i in range(obj2.length):
             if not np.array_equal(obj1.data[i], obj2.data[i]):
                 output_str = matrix_mismatch_string_builder(obj2.data[i], obj1.data[i])
                 self.fail(output_str)
 
+    def test_pose_so3_constructor_rot_matrix(self):
+        rot = tr.rotx(45, 'deg')
+        obj = pose.SO3(rot)
+        if not np.array_equal(rot, obj.data[0]):
+            output_str = matrix_mismatch_string_builder(obj.data[0], rot)
+            self.fail(output_str)
 
-    def test_pose_so3_constructor_se3(self):
-        self.fail("Missing implementation")
+    def test_pose_so3_constructor_se3_length(self):
+        objse3 = pose.SE3()
+        objso3 = pose.SO3(objse3)
+        self.assertEquals(objse3.length, objso3.length)
+
+    def test_pose_so3_constructor_se3_data(self):
+        objse3 = pose.SE3()
+        objso3 = pose.SO3(objse3)
+        rot_mats = []
+        for each in objse3:
+            rot_mats.append(tr.t2r(each))
+        for i in range(objso3.length):
+            if not np.array_equal(rot_mats[i], objso3.data[i]):
+                output_str = matrix_mismatch_string_builder(objso3.data[i], rot_mats[i])
+                self.fail(output_str)
+
+    def test_pose_so3_constructor_rx(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
