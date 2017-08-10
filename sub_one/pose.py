SO3 - rotation, t_matrix and det() added. Fix imports in the package

sub_one/pose.py
@@ -328,7 +328,10 @@ class SE2(SO2):
 
     def t_matrix(self):
         """Returns list of translation matrices of SE2 object"""
-        return self._list
+        if self.length == 1:
+            return self._list[0]
+        elif self.length > 1:
+            return self._list
 
     def inv(self):
         """Returns an inverse SE2 object of same length"""
@@ -500,6 +503,28 @@ class SO3(SuperPose):
         rotation = transforms.rpy2r(thetas=thetas, order=order, unit=unit)
         obj = cls(rotation)
         return obj
+
+    def rotation(self):
+        return self.mat
+
+    def t_matrix(self):
+        mat = []
+        for each in self:
+            mat.append(transforms.r2t(each))
+
+        if self.length == 1:
+            return mat[0]
+        elif self.length > 1:
+            return mat
+
+    def det(self):
+        det_list = []
+        for each in self:
+            det_list.append(np.linalg.det(each))
+        if self.length == 1:
+            return det_list[0]
+        elif self.length > 1:
+            return det_list
 
 
 # ---------------------------------------------------------------------------------

sub_one/tests/test_pose.py
@@ -171,7 +171,36 @@ class TestSO3(unittest.TestCase):
         self.fail("Not implemented yet")
 
     def test_pose_so3_det(self):
-        obj = pose.SO3()
+        obj = pose.SO3.Rx(theta=80, unit='deg')
+        det = obj.det()
+        if det != 1:
+            self.fail("Expected determinant value: 1.\n"
+                      "Received determinant value: det")
+
+    def test_pose_so3_rotation(self):
+        obj = pose.SO3.Rx(theta=45, unit='deg')
+        exp_rot = tr.rotx(theta=45, unit='deg')
+        rec_rot = obj.rotation()
+        if not matrices_equal(rec_rot, exp_rot):
+            output_str = matrix_mismatch_string_builder(rec_rot, exp_rot)
+            self.fail(output_str)
+
+    def test_pose_so3_rotation_list(self):
+        obj = pose.SO3.Rx(theta=[70, 80], unit='deg')
+        rot = [0, 0]
+        rot[0] = tr.rotx(theta=70, unit='deg')
+        rot[1] = tr.rotx(theta=80, unit='deg')
+        for i in range(obj.length):
+            if not matrices_equal(obj.data[i], rot[i]):
+                output_str = matrix_mismatch_string_builder(obj.data[i], rot[i])
+                self.fail(output_str)
+
+    def test_pose_so3_tr_matrix(self):
+        obj = pose.SO3.Rx(theta=45, unit='deg')
+        mat = np.matrix([[1.0000, 0, 0, 0], [0, 0.7071, -0.7071, 0], [0, 0.7071, 0.7071, 0], [0, 0, 0, 1.0000]])
+        if not matrices_equal(obj.t_matrix(), mat, decimal=4):
+            output_str = matrix_mismatch_string_builder(obj.t_matrix(), mat)
+            self.fail(output_str)
 
     def test_pose_so3_eig(self):
         self.fail("Not implemented yet")