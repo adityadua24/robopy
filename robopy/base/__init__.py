c8a9184bdfae3a9f98457f3fa8277e4c9f0c3c8f
Aditya Dua <adityadua24@gmail.com>
2018-04-27 01:05:33 +1000

    Optimised imports

--- robopy/base/__init__.py
+++ robopy/base/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ["util", "pose", "model", "transforms", "graphics", "common", "quaternion", "serial_link"]
+from robopy.base.transforms import *
+from robopy.base.model import *
+from robopy.base.quaternion import *
+from robopy.base.pose import *
+from robopy.base.util import *
