323f63900b6922540b1e68b158379505375ce617
Daniel Ingram <ingramds@appstate.edu>
2018-12-28 12:05:06 -0500

    Add more quaternion tests

--- robopy/tests/test_quaternion.py
+++ robopy/tests/test_quaternion.py
@@ -22,5 +22,56 @@ class TestQuaternion(unittest.TestCase):
                 q.v, exp_vec)
             self.fail(output_str)
 
+    def test_quaternion_constructor_with_scalar(self):
+        exp_sca = uniform(-1, 1)
+        q = Quaternion(exp_sca)
+        self.assertEqual(q.s, exp_sca)
+
+    def test_quaternion_constructor_with_vector(self):
+        exp_vec = np.matrix([[uniform(-50, 50) for _ in range(3)]])
+        q = Quaternion(v=exp_vec)
+        if not matrices_equal(q.v, exp_vec):
+            output_str = matrix_mismatch_string_builder(
+                q.v, exp_vec)
+            self.fail(output_str)
+
+    def test_quaternion_constructor_with_scalar_and_vector(self):
+        exp_sca = uniform(-1, 1)
+        exp_vec = np.matrix([[uniform(-50, 50) for _ in range(3)]])
+        q = Quaternion(exp_sca, exp_vec)
+        self.assertEqual(q.s, exp_sca)
+        if not matrices_equal(q.v, exp_vec):
+            output_str = matrix_mismatch_string_builder(
+                q.v, exp_vec)
+            self.fail(output_str)
+
+    def test_quaternion_qt_constructor(self):
+        q1 = Quaternion()
+        q2 = Quaternion.qt(q1)
+        self.assertEqual(q1.s, q2.s)
+        if not matrices_equal(q1.v, q2.v):
+            output_str = matrix_mismatch_string_builder(
+                q1.v, q2.v)
+            self.fail(output_str)
+
+    def test_quaternion_pure_constructor(self):
+        exp_vec = np.matrix([[uniform(-50, 50) for _ in range(3)]])
+        q = Quaternion.pure(exp_vec)
+        self.assertEqual(q.s, 0)
+        if not matrices_equal(q.v, exp_vec):
+            output_str = matrix_mismatch_string_builder(
+                q.v, exp_vec)
+            self.fail(output_str)
+
+    def test_quaternion_double(self):
+        exp_vec = np.matrix([[uniform(-1, 1) for _ in range(4)]])
+        q = Quaternion(float(exp_vec[0, 0]), exp_vec[0, 1:])
+        rec_vec = q.double()
+        if not matrices_equal(rec_vec, exp_vec):
+            output_str = matrix_mismatch_string_builder(
+                rec_vec, exp_vec)
+            self.fail(output_str)
+
+
 if __name__ == '__main__':
     unittest.main()
