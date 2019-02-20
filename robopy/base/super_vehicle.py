3ae33d96f8eac9142488219e24bf60193a2014ed
Daniel Ingram <ingramds@appstate.edu>
2019-02-19 20:22:23 -0500

    Add methods to Vehicle class

--- robopy/base/super_vehicle.py
+++ robopy/base/super_vehicle.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
+from math import sqrt, cos, sin, pi
+import numpy as np
 
 class Vehicle(ABC):
-    def __init__(self, x0=None, dt=0.1, L=1.0, max_speed=1.0, rdim=0.2, covar=None):
+    def __init__(self, x0=None, dt=0.1, L=1.0, max_speed=1.0, max_steer=pi/2, rdim=0.2, covar=None):
         """
         Initializes Vehicle object that implements the kinematic mode
         of a wheeled vehicle.
@@ -16,17 +18,75 @@ class Vehicle(ABC):
         self.dt = dt
         self.L = L
         self.max_speed = max_speed
+        self.max_steer = max_steer
         self.rdim = rdim
-        self.covar = [[0, 0], [0, 0]] if covar is None else covar
-        self.hist = []
+        self.V = [[0, 0], [0, 0]] if covar is None else covar
+        self.x_hist = []
 
     def init(self, x0=None):
         """
-        Reset vehicle state and clear history
+        Reset vehicle state and clear history.
         :param x0: State to which to set vehicle. Default is the original x0.
         """
         if x0 is None:
             self.x = self.x0[:]
         else:
             self.x = x0[:]
-        self.hist = []
+        self.x_hist = []
+
+    def add_driver(self, driver):
+        """
+        Add driver for the vehicle.
+        :param driver: Driver to add to the vehicle
+        """
+        self.driver = driver
+        driver.veh = self
+
+    def update(self, u):
+        """
+        Update vehicle state.
+        :param u: Control input [speed, steer]
+        :return odo: True odometry value
+        """
+        prev = self.x[:]
+        self.x[0] += u[0] * cos(self.x[2]) * self.dt
+        self.x[1] += u[0] * sin(self.x[2]) * self.dt
+        self.x[2] += u[0] * u[1] * self.dt / self.L
+        odo = [sqrt((self.x[0] - prev[0])**2 + (self.x[1] - prev[1])**2)]
+        odo.append(self.x[2] - prev[2])
+        self.odometry = odo
+        self.x_hist.append(self.x)
+        return odo
+
+    def step(self, u=None):
+        """
+        Advance one time step
+        :param u: Control input [speed, steer]
+        :return odo: Noisy odometry
+        """
+        u = self.control(u)
+        odo = self.update(u)
+        rand = np.random.randn(2, 1)
+        odo[0] += rand[0, 0] * self.V[0][0] + rand[1, 0] * self.V[0][1]
+        odo[1] += rand[0, 0] * self.V[0][1] + rand[1, 0] * self.V[1][1]
+        return odo
+
+    def control(self, u=None):
+        """
+        Compute the control input taking into account speed and
+        steering limits. If no control input is applied, one will
+        be demanded from the driver. If there is no driver, a control
+        input of [0, 0] is returned.
+        :param u: Control input [speed, steer]. Defaults to None.
+        :return u: Clipped control input.
+        """
+        if u is None:
+            if hasattr(self, 'driver'):
+                speed, steer = self.driver.demand()
+            else:
+                speed, steer = 0, 0
+        else:
+            speed, steer = u[0], u[1]
+        speed = min(self.max_speed, max(-self.max_speed, speed))
+        steer = max(-self.max_steer, min(self.max_steer, steer))
+        return [speed, steer]
