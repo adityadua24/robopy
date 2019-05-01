bde614d1e6315de9976e4769b982595a11f26a15
hkk0x26 <50078831+hkk0x26@users.noreply.github.com>
2019-05-01 09:53:36 +0530

    passing the np.matrix for pose

--- examples/puma560_plot.py
+++ examples/puma560_plot.py
@@ -1,9 +1,24 @@
 import robopy.base.model as model
-
+import numpy as np
 
 def main():
     robot = model.Puma560()
-    robot.plot('qn')
+    
+    '''zero angle'''
+    qn = np.matrix([0,0,0,0,0,0])
+    robot.plot(qn)
+    
+    '''ready, the arm is straight and vertical''
+    qn = np.matrix([0,np.pi/2,-np.pi/2,0,0,0])
+    robot.plot(qn)
+    
+    '''stretch, the arm is straight and horizontal'''
+    qn = np.matrix([0,0,-np.pi/2,0,0,0])
+    robot.plot(qn)
+    
+    '''nominal, the arm is in a dextrous working pose'''
+    qn = np.matrix([0,(np.pi/4),-np.pi,0,(np.pi/4),0])
+    robot.plot(qn)
 
 
 if __name__ == '__main__':
